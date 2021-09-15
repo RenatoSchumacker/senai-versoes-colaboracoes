60f528dfea57d51f0a0a569bf42d204fafd88d6d
RenatoSchumacker <89883735+RenatoSchumacker@users.noreply.github.com>
2021-09-15 20:00:11 -0300

    Add files via upload

---
+++ pecas.js
@@ -0,0 +1,27 @@
+const listaDePecas = ["Peca de teste", "ABCD", "Peca B"]
+
+console.log("quantidade de caracteres")
+
+if (listaDePecas.length <= 10) {
+    console.log("as peças podem ser cadastradas");
+}
+
+console.log("quantidade de caracteres")
+
+for (let index = 0; index < listaDePecas.length; index++) {
+    const pecaAtual = listaDePecas[index];
+    if (pecaAtual.length < 3) {
+        console.log(pecaAtual + ": a peça possui nome inferior a 3 caracteres e não pode ser cadastrada.")
+    } else {
+        console.log(pecaAtual + ": a peça pode ser cadastrada.") 
+    }
+}
+
+console.log("peso da peça")
+const pesoDaPecaEmGramas = 50;
+
+if (pesoDaPecaEmGramas >= 100) {
+    console.log("peso suficiente.")
+} else {
+    console.log("valor insuficiente")
+}
